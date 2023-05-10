d1d723bd63e7cd8563fdea7e2eb8bd14dd5fcf3c
Ian Neal <wopr@wopr.tech>
2023-05-09 22:29:20 -0600

    initial commit of madness

---
+++ openshift_ldap_operator/resources/__init__.py
@@ -0,0 +1,33 @@
+
+
+"""
+LdapAdminGroup
+  Spec
+    GroupRef
+    RoleRef
+  Status
+    GroupName
+    GroupDN
+    RoleRef
+LdapConfig
+  Spec
+    ServerConfig
+      Url
+      BindDN
+      BindPassword
+      Insecure
+      CA
+      BaseDN
+      ServerType
+        RFC2307
+        ActiveDirectory
+        AugmentedActiveDirectory
+    UserSearch
+    
+    GroupSearch
+    Schedule
+  Status
+    Conditions
+    LastUpdatedTime
+"""
+
