20ac535854d236ba37c2e116fdae5b1cc633daf5
Ian Neal <wopr@wopr.tech>
2023-05-13 13:50:49 -0600

    even more wip

--- openshift_ldap_operator/resources/config.py
+++ openshift_ldap_operator/resources/config.py
@@ -1,15 +1,36 @@
 from dataclasses import dataclass, field
-from typing import Optional
 from uuid import UUID
-from kubecrd import KubeResourceBase
+from typing import Optional
+from kubecrd import KubeResourceBase # maybe try something else though because we can't add status to these
 from apischema import schema
 
+
+@dataclass
+class K8sDataRef:
+    name: str = field(
+        metadata=schema(
+
+        )
+    )
+    key: str = field(
+        metadata=schema(
+
+        )
+    )
 
 
 @dataclass
 class LdapConfigPassword:
-    value: Optional[str]
-    secret: Optional
+    value: Optional[str] = field(
+        metadata=schema(
+            
+        )
+    )
+    secret: K8sDataRef = field(
+        metadata=schema(
+
+        ),
+    )
 
 @dataclass
 class LdapConfigCfgMap:
