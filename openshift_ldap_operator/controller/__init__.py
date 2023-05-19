2521056b039cb5dc9c25d8bfa753b830ba00f699
Ian Neal <wopr@wopr.tech>
2023-05-19 10:41:36 -0600

    🎉 begin work on controller

--- openshift_ldap_operator/controller/__init__.py
+++ openshift_ldap_operator/controller/__init__.py
@@ -1,2 +1,11 @@
 import kopf
 
+# Kopf doesn't require a main function or anything like that, so we just start
+# with declaring handlers!
+
+
+@kopf.on.login()  # type: ignore  # Apparently Pylance is gonna hate kopf...
+def login_fn(**kwargs):
+    return kopf.login_with_service_account(**kwargs)
+
+
